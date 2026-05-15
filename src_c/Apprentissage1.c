81923930847df2adbe75af4c2e8a034d20f0c572
dilhan.d <dilhan.dincer@epitech.eu>
2026-05-15 16:41:52 +0200

    Update V2_C techno

--- src_c/Apprentissage1.c
+++ src_c/Apprentissage1.c
@@ -1,3 +1,6 @@
 int main() {
 printf("Hello c est dilhan nous somme le 15/05/2026. \n Voici mes débuts en C :D "); 
-}
+
+int a = 5;
+int b = 10;
+printf("La somme de %d et %d est %d\n", a, b, a + b);}
