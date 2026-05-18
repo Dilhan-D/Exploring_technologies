5962eccd89f0a2865059fd94c5403b5c70d5f674
dilhan.d <dilhan.dincer@epitech.eu>
2026-05-18 17:32:05 +0200

    Update apprentissage

--- src_c/Apprentissage1.c
+++ src_c/Apprentissage1.c
@@ -1,6 +1,15 @@
 int main() {
 printf("Hello c est dilhan nous somme le 15/05/2026. \n Voici mes débuts en C :D "); 
 
+char construction1[35] = "Salut je me présente je m'appelle";
+char construction2[35] = "Et moi je me présente je m'appelle";
+char name1[10] = "Dilhan";
+char name2[10] = "John Elton";
+char name3[10] = "Mickael";
+
+//printf(construction1 + name3);
+//printf('%s %s\n', construction1, name1);
+
 int a = 5;
 int b = 10;
 printf("La somme de %d et %d est %d\n", a, b, a + b);}
