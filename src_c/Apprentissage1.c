4b708100ff1c42874ca54e7755cd3012ee939743
dilhan.d <dilhan.dincer@epitech.eu>
2026-05-18 17:33:02 +0200

    Update apprentissage

--- src_c/Apprentissage1.c
+++ src_c/Apprentissage1.c
@@ -6,6 +6,7 @@ char construction2[35] = "Et moi je me présente je m'appelle";
 char name1[10] = "Dilhan";
 char name2[10] = "John Elton";
 char name3[10] = "Mickael";
+float age1 = 20.5;
 
 //printf(construction1 + name3);
 //printf('%s %s\n', construction1, name1);
